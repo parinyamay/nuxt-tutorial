7dfee7e3eddc84abed600d19826a1461dc24594e
ParinyaWante <parinyaw@8interactive.co.th>
2023-03-16 15:37:51 +0700

    lesson: AsyncData and Promise

--- pages/admin/posts/index.vue
+++ pages/admin/posts/index.vue
@@ -12,46 +12,27 @@ import PostList from '@/components/posts/PostList'
 export default {
     components: { PostList },
     asyncData(context) {
-        return axios
-            .get('https://nuxt-tutorial-1f803-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
-            .then((res) => {
-                console.log(res.data)
-                const data = []
-                for (const key in res.data) {
-                    data.push({ ...res.data[key], id: key })
-                }
-                return {
-                    loadData: data,
-                }
-            })
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const data = axios
+                    .get('https://nuxt-tutorial-1f803-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
+                    .then((res) => {
+                        const postsData = []
+                        for (const key in res.data) {
+                            postsData.push({ ...res.data[key], id: key })
+                        }
+                        return {
+                            loadData: postsData,
+                        }
+                    })
+                resolve(data)
+            }, 4000)
+        })
     },
     data() {
         return {
             loadData: [],
         }
     },
-    // Async Await and Promise
-    // methods: {
-    //     async postsAll() {
-    //         console.log('loading...')
-    //         await this.getData().then((result) => {
-    //             console.log(result)
-    //         })
-    //         console.log('loaded...')
-    //     },
-
-    //     getData() {
-    //         return new Promise((resolve, reject) => {
-    //             setTimeout(() => {
-    //                 const res = axios
-    //                     .get('https://nuxt-tutorial-1f803-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json')
-    //                     .then((res) => {
-    //                         return res
-    //                     })
-    //                 resolve(res)
-    //             }, 4000)
-    //         })
-    //     },
-    // },
 }
 </script>
